71b1794f7f9020464dda54a46bbacec0c5614a49
Fahima Bushra <bushra.arifeen@gmail.com>
2024-05-21 23:53:22 -0700

    google sign in is not working

--- src/components/login-registration/GoogleLogin.jsx
+++ src/components/login-registration/GoogleLogin.jsx
@@ -1,9 +1,15 @@
 import googleIcon from "../../assets/images/googleIcon.jpg";
+import useAuth from "../../hooks/useAuth";
 
 const GoogleLogin = () => {
+    const {googleLogin} = useAuth();
+
+    const handleGoogleSignIn = () =>{
+        googleLogin()
+    }
     return (
         <div>
-            <button className="btn w-full bg-slate-300">
+            <button onClick={handleGoogleSignIn} className="btn w-full bg-slate-300">
                 <div className="flex items-center gap-2">
                     <img className="w-4" src={googleIcon} alt="" />
                     <p className="font-bold">Google</p>
